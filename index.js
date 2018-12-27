05796e722b8299944bf90794c8b26f671d33a5b9
Nikka Hassan <nikkamhassan@gmail.com>
2018-12-27 18:07:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function add2(n) {
    return n + two;
  }
 var funkyFunction = function() {
-  return 
+  return function
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
